faa36eaa1b3c719739a0604d2b3c68ff0fbf1192
JackySC004 <luanasosa004@gmail.com>
2024-02-29 19:34:03 +0700

    operators homework

---
+++ script.js
@@ -0,0 +1,15 @@
+//TAREA OPERADORES
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+if (num1 > num2  && num1 > num3){
+    console.log("El numero mayor es: ",num1)
+}
+if (num2 > num1  && num2 > num3){
+    console.log("El numero mayor es: ",num2)
+}
+else{
+    console.log("El numero mayor es: ",num3)
+}
